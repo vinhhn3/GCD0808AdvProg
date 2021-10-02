4729d1a382e489e7a384346337ab8fa1709f288d
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-02 17:13:58 +0700

    2. Salary Increase

--- Encapsulation.Lab/Person.cs
+++ Encapsulation.Lab/Person.cs
@@ -6,20 +6,35 @@
     public string LastName { get; private set; }
     public int Age { get; private set; }
 
+    public decimal Salary { get; private set; }
+
     public Person()
     {
     }
 
-    public Person(string firstName, string lastName, int age)
+    public Person(string firstName, string lastName, int age, decimal salary)
     {
       FirstName = firstName;
       LastName = lastName;
       Age = age;
+      Salary = salary;
     }
 
     public override string ToString()
     {
-      return $"{FirstName} {LastName} is {Age} years old!";
+      return $"{FirstName} {LastName} is {Age} years old, receives ${Salary} ";
+    }
+
+    public void IncreaseSalary(decimal percentage)
+    {
+      if (Age < 30)
+      {
+        Salary = Salary + Salary * (percentage / 200);
+      }
+      else
+      {
+        Salary = Salary + Salary * (percentage / 100);
+      }
     }
   }
 }
