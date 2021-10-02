e7fc688ec3800978a2232b2d74dd646c089587ed
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-02 17:42:46 +0700

    4. First and Reserve Team

--- Encapsulation.Lab/Program.cs
+++ Encapsulation.Lab/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Encapsulation.Lab
 {
@@ -10,6 +6,17 @@ namespace Encapsulation.Lab
   {
     static void Main(string[] args)
     {
+      Team team = new Team("A-Team");
+      team.AddPlayer(new Person("John", "Lennon", 50, 1000));
+      team.AddPlayer(new Person("Paul", "Vic", 35, 2000));
+      team.AddPlayer(new Person("George", "Winston", 39, 1000));
+      team.AddPlayer(new Person("James", "Bond", 60, 2000));
+      team.AddPlayer(new Person("Axl", "Rose", 23, 1000));
+
+      team.PrintInfo();
+
+      Console.ReadLine();
+
     }
   }
 }
---
+++ Encapsulation.Lab/Team.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Encapsulation.Lab
+{
+  class Team
+  {
+    private string _name;
+    private List<Person> _firstTeam;
+    private List<Person> _reserveTeam;
+    public IReadOnlyList<Person> FirstTeam
+    {
+      get
+      {
+        return _firstTeam.AsReadOnly();
+      }
+    }
+    public IReadOnlyList<Person> ReserveTeam
+    {
+      get
+      {
+        return _reserveTeam.AsReadOnly();
+      }
+    }
+
+    public Team(string name)
+    {
+      _name = name;
+      _firstTeam = new List<Person>();
+      _reserveTeam = new List<Person>();
+    }
+
+    public void AddPlayer(Person person)
+    {
+      if (person.Age < 40)
+      {
+        _firstTeam.Add(person);
+      }
+      else
+      {
+        _reserveTeam.Add(person);
+      }
+    }
+
+    public void PrintInfo()
+    {
+      Console.WriteLine($"Team name: {_name}");
+      Console.WriteLine("FIRST TEAM");
+      Console.WriteLine("Number of players: " + _firstTeam.Count);
+      foreach (var player in _firstTeam)
+      {
+        Console.WriteLine($"{player.FirstName} {player.LastName} | Age: {player.Age}");
+      }
+
+      Console.WriteLine("RESERVE TEAM");
+      Console.WriteLine("Number of players: " + _reserveTeam.Count);
+      foreach (var player in _reserveTeam)
+      {
+        Console.WriteLine($"{player.FirstName} {player.LastName} | Age: {player.Age}");
+      }
+    }
+  }
+}
