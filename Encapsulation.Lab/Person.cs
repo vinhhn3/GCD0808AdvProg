b7e5d069d7d29b2d52ba0c0b497dcf26fdc3f986
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-02 17:22:14 +0700

    3. Validation of Data

--- Encapsulation.Lab/Person.cs
+++ Encapsulation.Lab/Person.cs
@@ -1,12 +1,74 @@
-﻿namespace Encapsulation.Lab
+﻿using System;
+
+namespace Encapsulation.Lab
 {
   class Person
   {
-    public string FirstName { get; private set; }
-    public string LastName { get; private set; }
-    public int Age { get; private set; }
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+    private decimal _salary;
 
-    public decimal Salary { get; private set; }
+    public string FirstName
+    {
+      get
+      {
+        return _firstName;
+      }
+      set
+      {
+        if (value.Length < 3)
+        {
+          throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+        }
+        _firstName = value;
+      }
+    }
+    public string LastName
+    {
+      get
+      {
+        return _lastName;
+      }
+      set
+      {
+        if (value.Length < 3)
+        {
+          throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+        }
+        _lastName = value;
+      }
+    }
+    public int Age
+    {
+      get
+      {
+        return _age;
+      }
+      set
+      {
+        if (value <= 0)
+        {
+          throw new ArgumentException("Age cannot be zero or a negative integer!");
+        }
+        _age = value;
+      }
+    }
+    public decimal Salary
+    {
+      get
+      {
+        return _salary;
+      }
+      set
+      {
+        if (value < 460)
+        {
+          throw new ArgumentException("Salary cannot be less than 460 leva!");
+        }
+        _salary = value;
+      }
+    }
 
     public Person()
     {
