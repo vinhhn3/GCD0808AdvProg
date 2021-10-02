4aaa83d3d3e283c796e76347d18788e9f5417abd
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-02 16:54:02 +0700

    5. Stack of Strings

---
+++ Inheritance.Lab/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Inheritance.Lab
+{
+  class StackOfStrings : Stack<string>
+  {
+    public StackOfStrings()
+    {
+
+    }
+
+    public bool IsEmpty()
+    {
+      if (this.Count == 0) return true;
+      else return false;
+    }
+  }
+}
