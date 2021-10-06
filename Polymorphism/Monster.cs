cbe93b11c9201a7c9780f0d655df0056a6f354ea
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-06 14:14:29 +0700

    Overloading methods

---
+++ Polymorphism/Monster.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Polymorphism
+{
+  class Monster
+  {
+    public string FullName;
+    public int AttackPoint;
+
+    // Overloading methods: Constructor
+    public Monster()
+    {
+
+    }
+
+    public Monster(string fullName)
+    {
+      FullName = fullName;
+    }
+
+    public Monster(string fullName, int attackPoint)
+    {
+      FullName = fullName;
+      AttackPoint = attackPoint;
+    }
+
+    public void Attack()
+    {
+      Console.WriteLine($"Monster {FullName} is attacking");
+    }
+
+    public void Attack(string skill)
+    {
+      Console.WriteLine($"Monster {FullName} is attacking");
+      Console.WriteLine($"and using skill {skill}");
+    }
+
+    public string Attack(string skill, int repeat)
+    {
+      var result = $"Monster {FullName} is using skill {skill}" +
+        $" {repeat} times";
+      return result;
+    }
+
+  }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Polymorphism
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Monster monster = new Monster("Lich King");
+
+      monster.Attack(); // void
+      monster.Attack("Super Nova"); // void
+      Console.WriteLine(monster.Attack("Lighitng Fury", 10)); // string
+
+      Console.ReadLine();
+    }
+  }
+}
