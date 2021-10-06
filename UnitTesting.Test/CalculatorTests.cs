9e0e21a10224085115e6f97760f44d955902d82b
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-06 16:49:43 +0700

    Test Add function

---
+++ UnitTesting.Test/CalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace UnitTesting.Test
+{
+  [TestFixture]
+  class CalculatorTests
+  {
+    [Test]
+    public void Add_WhenCalled_ReturnTheSumOfArguments()
+    {
+      // AAA
+      // Arrange
+      Calculator cal = new Calculator();
+
+
+      // Act
+      var actualResult = cal.Add(1, 2);
+      var expectedResult = 3;
+
+      // Assert
+      Assert.AreEqual(expectedResult, actualResult);
+    }
+  }
+}
---
+++ UnitTesting.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8270e6ff-da16-4593-8924-8951ba2062fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
