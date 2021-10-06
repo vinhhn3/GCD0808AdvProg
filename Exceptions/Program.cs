525dc21d7f468af3051af0820fabb71b8a9f02d3
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-06 15:07:38 +0700

    Basic try ... catch ...

---
+++ Exceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exceptions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a = int.Parse(Console.ReadLine());
+      int b = int.Parse(Console.ReadLine());
+      int result = 0;
+
+      try
+      {
+        result = a / b;
+      }
+      catch (Exception)
+      {
+        Console.WriteLine("Cannot divide 0");
+      }
+
+      Console.WriteLine(result);
+
+      Console.ReadLine();
+
+    }
+  }
+}
