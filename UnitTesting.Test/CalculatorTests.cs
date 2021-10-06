dd404448b58a5d97adb9b1021970aaa693f32b9e
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-06 17:02:55 +0700

    Use multiple test cases in one function

--- UnitTesting.Test/CalculatorTests.cs
+++ UnitTesting.Test/CalculatorTests.cs
@@ -20,5 +20,21 @@ namespace UnitTesting.Test
       // Assert
       Assert.AreEqual(expectedResult, actualResult);
     }
+
+    [Test]
+    [TestCase(2, 2, 2)]
+    [TestCase(2, 1, 2)]
+    [TestCase(1, 2, 2)]
+    public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+    {
+      // Arrange
+      Calculator cal = new Calculator();
+
+      // Act 
+      var actualResult = cal.Max(a, b);
+
+      // Assert
+      Assert.AreEqual(expectedResult, actualResult);
+    }
   }
 }
