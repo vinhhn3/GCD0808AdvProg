8646657c522a61b7afcbdb28a92ac8ee7d480fd6
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-13 13:41:41 +0700

    SingletonPattern

---
+++ SingletonPattern/President.cs
@@ -0,0 +1,20 @@
+﻿namespace SingletonPattern
+{
+  class President
+  {
+    private static President _instance;
+    private President()
+    {
+
+    }
+
+    public static President GetInstance()
+    {
+      if (_instance == null)
+      {
+        _instance = new President();
+      }
+      return _instance;
+    }
+  }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      President a = President.GetInstance();
+      President b = President.GetInstance();
+
+      Console.WriteLine(a.Equals(b));   // true => only one President object is created
+
+      Console.ReadLine();
+    }
+  }
+}
